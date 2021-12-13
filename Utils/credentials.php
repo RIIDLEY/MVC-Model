c31671a5ac76f7920340542cf77cc31bc91c7afc
RIIDLEY <hamsek.lahoucine@gmail.com>
2021-12-13 10:53:27 +0100

    upload

---
+++ Utils/credentials.php
@@ -0,0 +1,5 @@
+<?php
+$dsn = "pgsql:host=aquabdd; dbname=etudiants";
+$login = "11813410";
+$mdp = "060893655DF";
+?>
---
+++ Views/view_home.php
@@ -0,0 +1,10 @@
+<?php
+require('view_begin.php');
+?>
+
+<h1> List of the nobel prizes </h1>
+
+
+<?php
+require('view_end.php');
+?>
